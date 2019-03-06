2c461bd096085d1c8f586af7a72258028fd05380
yaskovdev <yaskovdev@gmail.com>
2019-03-06 21:02:14 +0200

    Hide action bar.

--- app/src/main/java/com/motivepick/motive/MainActivity.kt
+++ app/src/main/java/com/motivepick/motive/MainActivity.kt
@@ -3,18 +3,14 @@ package com.motivepick.motive
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
-import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var toolbar: ActionBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        toolbar = supportActionBar!!
         val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
         bottomNavigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
@@ -24,19 +20,16 @@ class MainActivity : AppCompatActivity() {
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_tasks -> {
-                toolbar.title = "Tasks"
                 val songsFragment = TasksFragment.newInstance()
                 openFragment(songsFragment)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_schedule -> {
-                toolbar.title = "Schedule"
                 val albumsFragment = ScheduleFragment.newInstance()
                 openFragment(albumsFragment)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_account -> {
-                toolbar.title = "Account"
                 val artistsFragment = AccountFragment.newInstance()
                 openFragment(artistsFragment)
                 return@OnNavigationItemSelectedListener true
