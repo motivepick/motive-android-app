10c2ae96750ecf522902c7f3bebd7184e905771f
yaskovdev <yaskovdev@gmail.com>
2019-03-09 10:32:42 +0200

    Cleanup.

--- app/src/main/java/com/motivepick/motive/TaskViewItem.kt
+++ app/src/main/java/com/motivepick/motive/TaskViewItem.kt
@@ -1,8 +1,3 @@
 package com.motivepick.motive
 
-data class TaskViewItem(val name: String) {
-
-    fun getLabel(): String = name
-
-    fun isDone(): Boolean = false
-}
+data class TaskViewItem(val name: String)
--- app/src/main/java/com/motivepick/motive/TasksAdapter.kt
+++ app/src/main/java/com/motivepick/motive/TasksAdapter.kt
@@ -4,23 +4,9 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
 import android.widget.TextView
-import java.util.*
 
 class TasksAdapter(private val dataSet: List<TaskViewItem>) : RecyclerView.Adapter<TasksAdapter.ViewHolder>() {
-
-    private val doneItems = ArrayList<TaskViewItem>()
-
-    private var showCompletedTasks = true
-
-    init {
-        for (item in dataSet) {
-            if (item.isDone()) {
-                doneItems.add(item)
-            }
-        }
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.task, parent, false)
@@ -29,11 +15,7 @@ class TasksAdapter(private val dataSet: List<TaskViewItem>) : RecyclerView.Adapt
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val listItem = dataSet[position]
-        if (!listItem.isDone() || showCompletedTasks) {
-            holder.textView.setText(listItem.getLabel())
-            holder.textView.paint.isStrikeThruText = listItem.isDone()
-            holder.doneLink.visibility = if (listItem.isDone()) View.GONE else View.VISIBLE
-        }
+        holder.textView.text = listItem.name
     }
 
     override fun getItemCount(): Int {
@@ -42,14 +24,6 @@ class TasksAdapter(private val dataSet: List<TaskViewItem>) : RecyclerView.Adapt
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        val textView: TextView
-
-        val doneLink: ImageButton
-
-        init {
-            textView = itemView.findViewById(R.id.item_text) as TextView
-            doneLink = itemView.findViewById(R.id.item_done) as ImageButton
-        }
-
+        val textView: TextView = itemView.findViewById(R.id.item_text) as TextView
     }
 }
