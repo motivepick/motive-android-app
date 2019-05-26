d2ba996b8d69a78f1d07b783a9d53f81303986e4
yaskovdev <yaskovdev@gmail.com>
2019-05-26 12:36:39 +0300

    Cleanup code.

--- app/src/main/java/com/motivepick/motive/MainActivity.kt
+++ app/src/main/java/com/motivepick/motive/MainActivity.kt
@@ -3,7 +3,6 @@ package com.motivepick.motive
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 import com.motivepick.motive.account.AccountFragment
 import com.motivepick.motive.schedule.ScheduleFragment
@@ -15,7 +14,6 @@ class MainActivity : AppCompatActivity() {
     private val tasksFragment: TasksFragment = TasksFragment.newInstance()
     private val scheduleFragment: ScheduleFragment = ScheduleFragment.newInstance()
     private val accountFragment: AccountFragment = AccountFragment.newInstance()
-    private val fragmentManager: FragmentManager = supportFragmentManager
     private var active: Fragment = tasksFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,25 +21,25 @@ class MainActivity : AppCompatActivity() {
         supportActionBar!!.hide()
         setContentView(R.layout.activity_main)
         navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
-        fragmentManager.beginTransaction().add(R.id.container, accountFragment, "3").hide(accountFragment).commit()
-        fragmentManager.beginTransaction().add(R.id.container, scheduleFragment, "2").hide(scheduleFragment).commit()
-        fragmentManager.beginTransaction().add(R.id.container, tasksFragment, "1").commit()
+        supportFragmentManager.beginTransaction().add(R.id.container, accountFragment, "3").hide(accountFragment).commit()
+        supportFragmentManager.beginTransaction().add(R.id.container, scheduleFragment, "2").hide(scheduleFragment).commit()
+        supportFragmentManager.beginTransaction().add(R.id.container, tasksFragment, "1").commit()
     }
 
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_tasks -> {
-                fragmentManager.beginTransaction().hide(active).show(tasksFragment).commit()
+                supportFragmentManager.beginTransaction().hide(active).show(tasksFragment).commit()
                 active = tasksFragment
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_schedule -> {
-                fragmentManager.beginTransaction().hide(active).show(scheduleFragment).commit()
+                supportFragmentManager.beginTransaction().hide(active).show(scheduleFragment).commit()
                 active = scheduleFragment
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_account -> {
-                fragmentManager.beginTransaction().hide(active).show(accountFragment).commit()
+                supportFragmentManager.beginTransaction().hide(active).show(accountFragment).commit()
                 active = accountFragment
                 return@OnNavigationItemSelectedListener true
             }
